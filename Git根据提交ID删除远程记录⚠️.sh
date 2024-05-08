e7598110a52fda579fb993142d82ababd5ebe142
jobs <jobs@gmail.com>
2024-05-08 22:32:51 +0800

    new.版本1

--- Git根据提交ID删除远程记录⚠️.sh
+++ Git根据提交ID删除远程记录⚠️.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# 放在 .git 文件夹同级
-# ❤️要回退远程版本,首先需要有远程服务器的删除代码的权限❤️
 # 获取当前文件夹路径
 current_directory=$(dirname "$(realpath "$0")")
 # 切换到当前目录
 cd "$current_directory"
+
+# 函数：回退到指定的提交ID版本
+rollback_to_commit() {
+    commit_id=$1
+    git reset "$commit_id"
+    git stash # 暂存修改
+    git push --force origin :"$commit_id" # 删除远程提交
+    git stash pop # 释放暂存的修改，开始修改代码
+    git add .
+    git commit -m "Revert to $commit_id"
+    git push origin HEAD
+}
+
 # 获取用户输入
-read -r -p "请输入要删除的远程提交的ID: " remote_commit_id
+read -p "请输入提交的ID: " input_commit_id
+
 # 检查是否有输入
-if [ -z "$remote_commit_id" ]; then
-    echo "未输入远程提交ID,脚本退出"
+if [ -z "$input_commit_id" ]; then
+    echo "输入的提交ID不能为空"
     exit 1
 fi
-# 创建一个新的提交来撤销指定的提交
-git revert "$remote_commit_id"
-# 强制推送到远程仓库,删除指定的提交
-git push origin "+$remote_commit_id^:refs/heads/main"
 
-echo "已删除远程提交ID为 $remote_commit_id 的提交"
+rollback_to_commit "$input_commit_id"
+echo "已删除提交ID为 $input_commit_id 的版本，并推送到远程仓库"
